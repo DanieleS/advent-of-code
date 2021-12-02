9e45fe7545f9bd5039e6c6f38f975e5f7999665c
Davide Specchia <davide.specchia@ynap.com>
2021-12-02 14:41:10 +0100

    AOC 2021 - Day 1 - Part 2

--- 2021/src/day1.rs
+++ 2021/src/day1.rs
@@ -1,17 +1,36 @@
 use std::fs::read_to_string;
 
-pub fn main() {
-    let input = read_to_string("./input/day1.txt").unwrap();
+pub fn load_input() -> Vec<i32> {
+    let input = read_to_string("input/day1.txt").expect("Failed to read input");
+    input
+        .lines()
+        .map(|line| line.parse::<i32>().unwrap())
+        .collect()
+}
+
+pub fn calculate_increases(input: &Vec<i32>) -> i32 {
     let mut increases = 0;
     let mut last_depth = i32::MAX;
-    for line in input.lines() {
-        let depth = line.parse::<i32>().unwrap();
-        if depth > last_depth {
+    for depth in input {
+        if depth > &last_depth {
             increases += 1;
         }
 
-        last_depth = depth;
+        last_depth = depth.clone();
     }
 
-    println!("increases: {}", increases);
+    increases
+}
+
+pub fn main() {
+    let input = load_input();
+
+    println!("Part 1: {}", calculate_increases(&input));
+
+    let windowed_input: Vec<i32> = input
+        .windows(3)
+        .map(|window| window.iter().sum::<i32>())
+        .collect();
+
+    println!("Part 2: {}", calculate_increases(&windowed_input));
 }
