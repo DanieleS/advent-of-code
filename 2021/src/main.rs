2ada451560cb1d3ce588dd17034b337f0441a992
Davide Specchia <dnl.s.1992@gmail.com>
2021-12-03 20:07:16 +0100

    Add Day selection menu

--- 2021/src/main.rs
+++ 2021/src/main.rs
@@ -1,5 +1,21 @@
 mod day1;
 
-fn main() {
-    day1::main();
+use dialoguer::Select;
+
+fn main() -> std::io::Result<()> {
+    let days = vec![day1::main];
+
+    let options: Vec<String> = days
+        .iter()
+        .enumerate()
+        .map(|(i, _)| format!("Day {}", i + 1))
+        .collect();
+
+    println!("Select the day to be executed:");
+
+    let selection = Select::new().items(&options).default(0).interact()?;
+
+    days[selection]();
+
+    Ok(())
 }
