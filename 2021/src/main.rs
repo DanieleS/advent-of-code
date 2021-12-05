51d6dda346f0a0330de2580bfe575848e203740b
Davide Specchia <dnl.s.1992@gmail.com>
2021-12-05 14:20:20 +0100

    AOC 2021 - Day 5

--- 2021/src/main.rs
+++ 2021/src/main.rs
@@ -2,11 +2,12 @@ mod day1;
 mod day2;
 mod day3;
 mod day4;
+mod day5;
 
 use dialoguer::Select;
 
 fn main() -> std::io::Result<()> {
-    let days = [day1::main, day2::main, day3::main, day4::main];
+    let days = [day1::main, day2::main, day3::main, day4::main, day5::main];
 
     let options: Vec<String> = days
         .iter()
