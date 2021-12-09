a9e0ce6e2c3dde3d4fb7f5bdb0ccf3affbbbcbbc
Davide Specchia <davide.specchia@ynap.com>
2021-12-09 10:41:23 +0100

    AOC 2021 - Day 6 - Part 2

--- 2021/src/day6.rs
+++ 2021/src/day6.rs
@@ -1,51 +1,43 @@
 use std::fs::read_to_string;
 
-#[derive(Debug, Clone)]
-struct Lanternfish {
-    timer: i8,
-}
+type Fishes = [u64; 9];
 
-impl Lanternfish {
-    fn new(timer: i8) -> Lanternfish {
-        Lanternfish { timer }
-    }
-
-    fn next(&mut self) -> Option<Lanternfish> {
-        self.timer -= 1;
-
-        if self.timer == -1 {
-            self.timer = 6;
-            Some(Lanternfish::new(8))
-        } else {
-            None
-        }
-    }
-}
-
-fn load_input() -> Vec<Lanternfish> {
+fn load_input() -> Fishes {
     let input = read_to_string("input/day6.txt").expect("Failed to read input");
 
-    input
-        .split(",")
-        .map(|n| n.parse::<i8>().unwrap())
-        .map(Lanternfish::new)
-        .collect()
-}
+    let mut fishes = [0; 9];
 
-fn part_1(input: Vec<Lanternfish>) {
-    let mut pool = input;
-
-    for _ in 0..80 {
-        let mut new_fishes: Vec<Lanternfish> = pool.iter_mut().filter_map(|f| f.next()).collect();
-
-        pool.append(&mut new_fishes);
+    for n in input.split(",") {
+        fishes[n.parse::<usize>().expect("Invalid input")] += 1;
     }
 
-    println!("Pool size: {}", pool.len());
+    fishes
+}
+
+fn simulate_fishes(fishes: Fishes, days: usize) -> u64 {
+    let mut fishes = fishes.clone();
+    for _ in 0..days {
+        fishes.rotate_left(1);
+        fishes[6] += fishes[8];
+    }
+    fishes.iter().sum()
+}
+
+fn part_1(input: Fishes) {
+    let result = simulate_fishes(input, 80);
+
+    println!("Part 1: {}", result);
+}
+
+fn part_2(input: Fishes) {
+    let result = simulate_fishes(input, 256);
+
+    println!("Part 2: {}", result);
 }
 
 pub fn main() {
     let input = load_input();
 
     part_1(input.clone());
+    part_2(input.clone());
 }
