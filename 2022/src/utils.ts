d2bed7c5fc1f6623a0bac776ae5378c0c53ab48b
Davide Specchia <davide.specchia@mirta.com>
2022-12-01 18:02:57 +0100

    AOC 2022 - Day 1

--- 2022/src/utils.ts
+++ 2022/src/utils.ts
@@ -1,4 +1,6 @@
 import { ExecutionResult } from "./types/executionResult";
+import { join } from "path";
+import { readFile } from "fs/promises";
 
 export function printResults(results: ExecutionResult) {
   console.log(`Results of Day ${results.day}`);
@@ -11,4 +13,12 @@ export function printResults(results: ExecutionResult) {
     console.log(`Puzzle 2:`);
     console.log(results.puzzle2);
   }
+}
+
+export async function loadInputFile(day: number): Promise<string[]> {
+  const inputFilePath = join(import.meta.dir, "./input", `day${day}.txt`);
+
+  const file = (await readFile(inputFilePath)).toString();
+
+  return file.split("\n");
 }
