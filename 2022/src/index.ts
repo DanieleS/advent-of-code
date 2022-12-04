e9ba89a0b05ef978841df03045fa603480ab4441
Davide Specchia <davide.specchia@mirta.com>
2022-12-04 13:12:04 +0100

    AOC 2022 - Day 4

--- 2022/src/index.ts
+++ 2022/src/index.ts
@@ -1,11 +1,13 @@
 import { day1 } from "./days/day1";
 import { day2 } from "./days/day2";
 import { day3 } from "./days/day3";
+import { day4 } from "./days/day4";
 import { ExecutionResult } from "./types/executionResult";
 import { printResults } from "./utils";
 
 // const result: ExecutionResult = await day1();
 // const result: ExecutionResult = await day2();
-const result: ExecutionResult = await day3();
+// const result: ExecutionResult = await day3();
+const result: ExecutionResult = await day4();
 
 printResults(result);
