a58ee0b73af86e789a6c46c95492c9f6dcbe29b5
Davide Specchia <dnl.s.1992@gmail.com>
2023-12-02 19:44:28 +0100

    AOC 2023 - Day 1

---
+++ 2023/src/days/day1.ts
@@ -0,0 +1,66 @@
+import { ExecutionResult } from "../types/executionResult";
+import { loadInputFile } from "../utils";
+
+function solvePuzzle1(input: string[]): number {
+  const firstDigitRegex = /^[^\d]*(\d)/;
+  const lastDigitRegex = /(\d)[^\d]*$/;
+  const parseRow = (row: string) => {
+    const firstDigit = firstDigitRegex.exec(row)![1];
+    const lastDigit = lastDigitRegex.exec(row)![1];
+
+    return parseInt(firstDigit + lastDigit);
+  };
+
+  return input.map(parseRow).reduce((acc, v) => acc + v, 0);
+}
+
+function toNumber(number: string) {
+  switch (number) {
+    case "one":
+      return "1";
+    case "two":
+      return "2";
+    case "three":
+      return "3";
+    case "four":
+      return "4";
+    case "five":
+      return "5";
+    case "six":
+      return "6";
+    case "seven":
+      return "7";
+    case "eight":
+      return "8";
+    case "nine":
+      return "9";
+    default:
+      return number;
+  }
+}
+
+function solvePuzzle2(input: string[]): number {
+  const digiWord = "one|two|three|four|five|six|seven|eight|nine";
+  const firstDigitRegex = new RegExp(`(\\d|${digiWord})`);
+  const lastDigitRegex = new RegExp(`.*(\\d|${digiWord})`);
+  const parseRow = (row: string) => {
+    const firstDigit = firstDigitRegex.exec(row)![1];
+    const lastDigit = lastDigitRegex.exec(row)![1];
+
+    return parseInt(toNumber(firstDigit) + toNumber(lastDigit));
+  };
+
+  return input.map(parseRow).reduce((acc, v) => acc + v, 0);
+}
+
+export async function day1(): Promise<ExecutionResult> {
+  const input = await loadInputFile(1);
+  const puzzle1 = solvePuzzle1(input);
+  const puzzle2 = solvePuzzle2(input);
+
+  return {
+    day: 1,
+    puzzle1,
+    puzzle2,
+  };
+}
---
+++ 2023/src/utils.ts
@@ -0,0 +1,41 @@
+import { ExecutionResult } from "./types/executionResult";
+import { join } from "path";
+import { readFile } from "fs/promises";
+
+export function printResults(results: ExecutionResult) {
+  console.log(`Results of Day ${results.day}`);
+  console.log();
+  console.log(`Puzzle 1:`);
+  console.log(results.puzzle1);
+
+  if (results.puzzle2) {
+    console.log();
+    console.log(`Puzzle 2:`);
+    console.log(results.puzzle2);
+  }
+}
+
+export async function loadInputFile(day: number): Promise<string[]> {
+  const inputFilePath = join(import.meta.dir, "./input", `day${day}.txt`);
+
+  const file = (await readFile(inputFilePath)).toString();
+
+  return file.split("\n");
+}
+
+export function memoized<I extends object, O>(
+  fn: (input: I) => O
+): (input: I) => O {
+  const results = new WeakMap<I, O>();
+  return (input) => {
+    const memoedResult = results.get(input);
+    if (memoedResult) {
+      return memoedResult;
+    }
+
+    const result = fn(input);
+    results.set(input, result);
+
+    return result;
+  };
+}
