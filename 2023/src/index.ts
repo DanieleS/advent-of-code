8ba411b89940ba4f973fc5e35471c321bab54bea
Davide Specchia <dnl.s.1992@gmail.com>
2023-12-02 19:44:28 +0100

    AOC 2023 - Day 1

---
+++ 2023/src/index.ts
@@ -0,0 +1,7 @@
+import { day1 } from "./days/day1";
+import { ExecutionResult } from "./types/executionResult";
+import { printResults } from "./utils";
+
+const result: ExecutionResult = await day1();
+
+printResults(result);
