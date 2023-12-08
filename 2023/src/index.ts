0d5cb4b725079c53e5f69cc4119ef6745a4c4acb
Davide Specchia <dnl.s.1992@gmail.com>
2023-12-08 23:24:31 +0100

    AOC 2023 - Day 8

--- 2023/src/index.ts
+++ 2023/src/index.ts
@@ -3,7 +3,8 @@ import { day2 } from "./days/day2";
 import { day3 } from "./days/day3";
 import { day4 } from "./days/day4";
 import { day5 } from "./days/day5";
-import { day6 } from "./days/day6";
+import { day7 } from "./days/day7";
+import { day8 } from "./days/day8";
 import { ExecutionResult } from "./types/executionResult";
 import { printResults } from "./utils";
 
@@ -12,6 +13,7 @@ import { printResults } from "./utils";
 // const result: ExecutionResult = await day3();
 // const result: ExecutionResult = await day4();
 // const result: ExecutionResult = await day5();
-const result: ExecutionResult = await day6();
+// const result: ExecutionResult = await day6();
+const result: ExecutionResult = await day8();
 
 printResults(result);
