d39c522313ad03ba154069bba851dc64c83fca13
Davide Specchia <davide.specchia@kampaay.com>
2024-12-10 10:25:14 +0100

    AOC 2024 - Day 10

---
+++ 2024/src/days/day10.ts
@@ -0,0 +1,92 @@
+import type { PuzzleSolver } from "@aoc/framework";
+import { sum } from "@aoc/framework/math";
+
+type Point = [x: number, y: number];
+
+function startingPositions(input: string[]): Point[] {
+  return input.flatMap((row, y) =>
+    row.split("").flatMap((cell, x) => {
+      if (cell === "0") {
+        return [[x, y] as Point];
+      }
+
+      return [];
+    })
+  );
+}
+
+function solvePuzzle1(input: string[]): number {
+  const positions = startingPositions(input);
+
+  const solveNextStep = (
+    position: Point,
+    foundNines = new Set<string>()
+  ): number => {
+    const currentHeight = input[position[1]][position[0]];
+
+    if (currentHeight === "9") {
+      if (foundNines.has(position.join())) {
+        // Already found this 9
+        return 0;
+      }
+
+      foundNines.add(position.join());
+      return 1;
+    }
+
+    const nextPositions = (
+      [
+        [position[0] - 1, position[1]],
+        [position[0] + 1, position[1]],
+        [position[0], position[1] - 1],
+        [position[0], position[1] + 1],
+      ] as Point[]
+    ).filter(
+      (position) =>
+        input[position[1]]?.[position[0]] === String(Number(currentHeight) + 1)
+    );
+
+    return nextPositions
+      .map((p) => solveNextStep(p, foundNines))
+      .reduce(sum, 0);
+  };
+
+  return positions.map((p) => solveNextStep(p)).reduce(sum, 0);
+}
+
+function solvePuzzle2(input: string[]): number {
+  const positions = startingPositions(input);
+
+  const solveNextStep = (position: Point): number => {
+    const currentHeight = input[position[1]][position[0]];
+
+    if (currentHeight === "9") {
+      return 1;
+    }
+
+    const nextPositions = (
+      [
+        [position[0] - 1, position[1]],
+        [position[0] + 1, position[1]],
+        [position[0], position[1] - 1],
+        [position[0], position[1] + 1],
+      ] as Point[]
+    ).filter(
+      (position) =>
+        input[position[1]]?.[position[0]] === String(Number(currentHeight) + 1)
+    );
+
+    return nextPositions.map(solveNextStep).reduce(sum, 0);
+  };
+
+  return positions.map(solveNextStep).reduce(sum, 0);
+}
+
+const solver: PuzzleSolver = async (input) => {
+  return {
+    puzzle1: solvePuzzle1(input),
+    puzzle2: solvePuzzle2(input),
+  };
+};
+
+export default solver;
